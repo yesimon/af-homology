74b5c5092945716fcc466147570d27724d98c63f
Simon Ye <sye737@gmail.com>
2011-12-06 16:48:36 -0800

    Add pkl to dat.

---
+++ pkl_to_dat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import cPickle as pickle
+import sys
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', type=argparse.FileType('rb'), default=sys.stdin)
+    OPTS = parser.parse_args()
+    cne_dict = pickle.load(OPTS.infile)
+
+    for cne, results in cne_dict.iteritems():
+        for coord, score in results:
+            sys.stdout.write('%s\t%s\n' % (cne, score))
+
+if __name__ == '__main__':
+    main()
